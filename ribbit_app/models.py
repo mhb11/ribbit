c8c10d11f28642ad517e65b0e57d41866e56e660
Hassan Baig <hassan.baig@alumni.duke.edu>
2015-06-20 01:47:16 -0700

    Created Models

--- ribbit_app/models.py
+++ ribbit_app/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
+import hashlib
+ 
+ 
+class Ribbit(models.Model): #like tweets
+    content = models.CharField(max_length=140)
+    user = models.ForeignKey(User) #a ForeignKey to the User model, so that we have a relation between the two models
+    creation_date = models.DateTimeField(auto_now=True, blank=True)
+ 
+ 
+class UserProfile(models.Model): #a subclass of User, just includes the extra attribute of "follows"
+    user = models.OneToOneField(User) #defines a One to One relation with the User Model
+    follows = models.ManyToManyField('self', related_name='followed_by', symmetrical=False)#implements follows and followed_by both
+ 
+    def gravatar_url(self): #defines a function to get the link to the gravatar image based upon the user's url
+        return "http://www.gravatar.com/avatar/%s?s=50" % hashlib.md5(self.user.email).hexdigest()
+ 
+ 
+User.profile = property(lambda u: UserProfile.objects.get_or_create(user=u)[0]) #a property to get (if the UserProfile exists for the user) or create one when we use the syntax <user_object>.profile
 
-# Create your models here.
+''' Here's an example of how to use above to get the users that a given User follows and is followed by:
+superUser = User.object.get(id=1)
+superUser.profile.follows.all() # Will return an iterator of UserProfile instances of all users that superUser follows
+superUse.profile.followed_by.all() # Will return an iterator of UserProfile instances of all users that follow superUser''' 
