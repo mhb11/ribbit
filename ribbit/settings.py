90a2ceda7c9d374a9bb4f9e1ab8c7e748814e555
Hassan Baig <hassan.baig@alumni.duke.edu>
2015-06-20 02:51:56 -0700

    correcting static_url

--- ribbit/settings.py
+++ ribbit/settings.py
@@ -88,7 +88,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, 'static') #Django finds static files from this directory
+STATIC_ROOT = 'staticfiles' #Django finds static files from this directory
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
