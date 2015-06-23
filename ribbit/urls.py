6f1b6633bb98b8bac2ab5bd19c8618fbffa3e6a2
Hassan Baig <hassan.baig@alumni.duke.edu>
2015-06-23 03:47:04 -0700

    added admin

--- ribbit/urls.py
+++ ribbit/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import patterns, include, url
-
+from django.contrib import admin
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
-# admin.autodiscover()
+admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
+    url(r'^admin/', include(admin.site.urls)),
     url(r'^$', 'ribbit_app.views.index'), # root
     url(r'^login$', 'ribbit_app.views.login_view'), # login
     url(r'^logout$', 'ribbit_app.views.logout_view'), # logout
